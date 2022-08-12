4c0181788feb5c4d51de744823bf47a918c5f624
k3csk3 <szilagyi.viktor89@gmail.com>
2022-08-12 17:37:50 +0200

    full basic

--- solutions/basic/sum-and-avg/sum-and-avg.js
+++ solutions/basic/sum-and-avg/sum-and-avg.js
@@ -1,7 +1,19 @@
-export function getSumOfTheElements (arr) {
 
+export function getSumOfTheElements (arr) {
+    let arrAllSum = 0
+    for (let i = 0; i < arr.length; i++) {
+        arrAllSum += arr[i]
+    }
+
+    return arrAllSum
 }
 
 export function getAvarageOfTheElements (arr) {
- 
+    return getSumOfTheElements(arr) / arr.length
+
 }
+
+
+    
+    
+    
